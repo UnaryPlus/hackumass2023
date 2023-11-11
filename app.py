006ef3636b2287bacc8fe6e9c67f4851d0e98a0a
ohosman04 <omargawad9000@gmail.com>
2023-11-11 14:00:36 -0500

    added average rating from db

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
  
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://team1:Y6IsKogedLQRMQD2@umassapp.amt2nke.mongodb.net/?retryWrites=true&w=majority")
+db = client["dining"]
+users = db["users"]
 
 @app.route("/")
 def main_page():
@@ -15,6 +20,16 @@ def main_page():
 @app.route("/dc/<name>")
 def dc_page(name):
     return render_template("dc.html")
- 
+
+def average_rating(hall_name):
+    hall = db[f"{hall_name}_comments"]
+    data = hall.find({}, {"rating": 1, "_id": 0})
+    
+    ratings = [doc["rating"] for doc in data if doc.get("rating") is not None]
+
+    average_rating = sum(ratings) / len(ratings)
+    
+    return average_rating
+
 if __name__ == "__main__":
     app.run(debug=True)   
