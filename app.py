9f560c0b9ebb4f092d5fe2a431adaa6cf2a70d8e
UnaryPlus <ombspring@gmail.com>
2023-11-11 15:35:20 -0500

    get averge stars

--- app.py
+++ app.py
@@ -5,31 +5,25 @@ app = Flask(__name__)
 
 client = MongoClient("mongodb+srv://team1:Y6IsKogedLQRMQD2@umassapp.amt2nke.mongodb.net/?retryWrites=true&w=majority")
 db = client["dining"]
-users = db["users"]
+
+def average_stars(dc):
+    data = db[dc + "_comments"].find({ "rating": { "$ne": None } })
+    ratings = [ doc["rating"] for doc in data ]
+    avg_stars = sum(ratings) / len(ratings)
+    return avg_stars
 
 @app.route("/")
 def main_page():
     dcs = [
-        { "name": "worcester", "avg_stars": 4 },
-        { "name": "hamp", "avg_stars": 2.6 },
-        { "name": "berk", "avg_stars": 4.55 },
-        { "name": "frank", "avg_stars": 1 },
+        { "name": name, "avg_stars": average_stars(name) }
+        for name in [ "woo", "hamp", "berk", "frank" ]
     ]
+    dcs.sort(key=lambda dc: -dc["avg_stars"])
     return render_template("index.html", dcs=dcs)
 
 @app.route("/dc/<name>")
 def dc_page(name):
     return render_template("dc.html")
-
-def average_rating(hall_name):
-    hall = db[f"{hall_name}_comments"]
-    data = hall.find({}, {"rating": 1, "_id": 0})
-    
-    ratings = [doc["rating"] for doc in data if doc.get("rating") is not None]
-
-    average_rating = sum(ratings) / len(ratings)
-    
-    return average_rating
 
 if __name__ == "__main__":
     app.run(debug=True)   
