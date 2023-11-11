78305a4b805f5db46cc9884a67a2089758a1383c
UnaryPlus <ombspring@gmail.com>
2023-11-11 12:58:05 -0500

    initial website stuff

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
  
 app = Flask(__name__)
- 
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def main_page():
+    dcs = [
+        { "name": "worcester", "avg_stars": 4 },
+        { "name": "hamp", "avg_stars": 2.6 },
+        { "name": "berk", "avg_stars": 4.55 },
+        { "name": "frank", "avg_stars": 1 },
+    ]
+    return render_template("index.html", dcs=dcs)
+
+@app.route("/dc/<name>")
+def dc_page(name):
+    return render_template("dc.html")
  
 if __name__ == "__main__":
     app.run(debug=True)   
